9638c4ead78a26a9c48d79d2832e849b28dac840
Alexis Castillo <alexisct629@Mac.vfc.com>
2024-11-13 22:18:40 -0500

    change app file because it was a having extra code

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 
 const { PORT = 3001 } = process.env;
 
-const mainRouter = require('./routes/index').default;
+const mainRouter = require('./routes/index')
 
 //DB connection
-mongoose.connect('mongodb://localhost:27017/wtwr_db').then(()=>{
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db').then(()=>{
   console.log("connected to DB")
 }).catch(console.error);
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const clothingRouter = require("./clothingItems");
 router.use("/users", userRouter);
 router.use("/items",clothingRouter );
 router.use((req, res) => {
-  res.status(500).send({ message: "Router not found" });
+  res.status(400).send({ message: "Router not found" });
 });
 
 module.exports =router;
